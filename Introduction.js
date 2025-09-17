b6331f293494b2cdb16ab3161b2fcbce5e5a03cb
farihaa1 <farihaa1403@gmail.com>
2025-09-17 20:28:05 +0600

    first commit

---
+++ Introduction.js
@@ -0,0 +1,8 @@
+// modular system
+// 1.Local Modules(we create)
+// 2.built in modules(come wth node.js)
+// 3.third party modules(created by other)
+
+// common js
+// exports from on file to another by require
+// const var = require("filename...filepath")
